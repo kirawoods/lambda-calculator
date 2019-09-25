0748e842951927078eb2f5bde58a55faaddc143f
Kira <kawoods826@gmail.com>
2019-09-25 09:36:48 -0700

    removed state from number labels

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { numbers } from '../../../data.js';
 import {NumberButton} from './NumberButton.js';
 
@@ -12,10 +12,10 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
- const [numberState] = useState(numbers);
+ const numberLabels = numbers;
   return (
     <div className = "numbers">
-      {numberState.map((numbers, index) => (
+      {numberLabels.map((numbers, index) => (
         <NumberButton key={index} numbers = {numbers} />
       ))}
     </div>
